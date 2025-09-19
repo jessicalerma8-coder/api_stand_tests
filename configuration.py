729ce70d3d7e508b3d7bdbcc38158516a1c95fe5
Jessica Lerma <jessicalerma8@gmail.com>
2025-09-18 19:52:36 -0600

    hola

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-643bbbc0-77ef-42f8-b42f-6ce403691b6e.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
